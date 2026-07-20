83a741ced95767ee444143adff8841b0329ab8ac
PhilipDeegan <philip.deegan@gmail.com>
2026-07-20 19:39:26 +0200

    printing only on rank 0 (#1250)

--- pyphare/pyphare/cpp/__init__.py
+++ pyphare/pyphare/cpp/__init__.py
@@ -2,6 +2,7 @@
 #
 #
 
+import os
 import json
 import importlib
 from . import validate
@@ -64,3 +65,25 @@ def mpi_size():
 
 def mpi_barrier():
     return getattr(cpp_etc_lib(), "mpi_barrier")()
+
+
+def mpi_initialized():
+    return getattr(cpp_etc_lib(), "mpi_initialized")()
+
+
+def print_rank0(*args, **kwargs):
+    def should_print():
+        try:
+            if mpi_initialized():
+                return mpi_rank() == 0
+        except ImportError:
+            # missing module or mpi not initialized
+            ...
+        envs = ["OMPI_COMM_WORLD_RANK", "SLURM_PROCID"]
+        for env in envs:
+            if env in os.environ:
+                return int(os.environ[env]) == 0
+        return True  # FALL BACK ALWAYS PRINT
+
+    if should_print():
+        print(*args, **kwargs)
